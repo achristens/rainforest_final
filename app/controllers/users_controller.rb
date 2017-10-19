ef3c0b7a62d29ba42fb1075e60fc66bbec656b9e
Abby Christens <achristens@gmail.com>
2017-10-19 11:44:16 -0400

    Adding sessions controller for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new
+    # this is traversing the way the info is saved, a key in a hash, which is a hash called user, and then the keys in user
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      # Use _url in controllers!
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 end
