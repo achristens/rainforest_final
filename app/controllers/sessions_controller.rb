70954232c94c3de506ba48f89c183eacdd7fecf5
Abby Christens <achristens@gmail.com>
2017-10-19 12:11:17 -0400

    Adding session logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,27 @@
 class SessionsController < ApplicationController
   def new
-    
+
   end
 
   def create
+    u = User.find_by(email: params[:session][:email])
+
+    # This is saying if the user was found with the above find_by AND the password is correct, then redirect to the homepage
+    if u && u.authenticate(params[:session][:password])
+
+      # The "wristband" aka cookie on the user using the session hash. It's a magic hash like params and flash
+      session[:user_id] = u.id
+
+      flash.notice = "You're logged in."
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def destroy
+    # Take off the "wristband"
+    session[:user_id] = nil
+    redirect_to root_url
   end
 end
