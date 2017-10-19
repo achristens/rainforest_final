94b67756d52324876fbe22b530694cf7b85ede47
Abby Christens <achristens@gmail.com>
2017-10-19 15:31:21 -0400

    Setting grid width

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,10 @@ class SessionsController < ApplicationController
 
   def create
     u = User.find_by(email: params[:session][:email])
-
     # This is saying if the user was found with the above find_by AND the password is correct, then redirect to the homepage
     if u && u.authenticate(params[:session][:password])
-
       # The "wristband" aka cookie on the user using the session hash. It's a magic hash like params and flash
       session[:user_id] = u.id
-
       redirect_to root_url
     else
       render :new
