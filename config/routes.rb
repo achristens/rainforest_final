8c281d1b672db6e40273bac196786d2659b11f28
Abby Christens <achristens@gmail.com>
2017-10-19 11:46:05 -0400

    Removing useless sessions views

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # This is a SINGULAR resource. You will not create an index route or an id route
   resource :users, only: %i(create new)
-
+  resource :sessions, only: %i(create new destroy)
+  
   resources :products do
     resources :reviews, except: [:index, :new, :show]
   end
